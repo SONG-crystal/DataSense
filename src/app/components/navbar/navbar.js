f38708a629ab9b1be9b14baf195cce50854393a6
Sujung Song <sujungsong0610@gmail.com>
2025-03-21 15:08:24 -0400

    add two lines in navbar

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -20,9 +20,9 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+    <nav >
+      <div className="max-w-8xl mx-auto">
+        <div className="flex justify-between h-23 py-4">
           {/* Logo */}
           <div className="flex-shrink-0 flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-800">
@@ -30,64 +30,8 @@ export default function Navbar() {
             </Link>
           </div>
 
-          {/* Mobile Menu Button */}
-          <div className="flex items-center sm:hidden">
-            <button
-              onClick={toggleMenu}
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
-            >
-              <span className="sr-only">Open main menu</span>
-              {/* Hamburger Icon */}
-              <svg
-                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              {/* Close Icon */}
-              <svg
-                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* Desktop Menu */}
-          <div className="hidden sm:flex sm:space-x-8 sm:items-center">
-            <Link
-              href="/"
-              className="text-gray-800 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              className="text-gray-800 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              About
-            </Link>
-            {/* Conditional Rendering for Authenticated Users */}
+          {/* 로그인/회원가입 버튼 */}
+          <div className="flex items-center space-x-4">
             {status === "authenticated" ? (
               <>
                 <Link
@@ -111,19 +55,83 @@ export default function Navbar() {
                 </button>
               </>
             ) : (
-              <Link
-                href="/login"
-                className="text-gray-800 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Login
-              </Link>
+              <>
+                <button onClick={() => window.location.href = '/login'}   class="rounded-full bg-transparent hover:bg-gray-600 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">
+                  Login
+                </button> 
+
+                <button onClick={() => window.location.href = '/signup'}  class="rounded-full bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 ">
+                  Get Started
+                </button>
+              </>
             )}
           </div>
         </div>
+
+          {/* Mobile Menu Button */}
+          {/* <div className="flex items-center sm:hidden">
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
+            >
+              <span className="sr-only">Open main menu</span> */}
+              {/* Hamburger Icon */}
+              {/* <svg
+                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg> */}
+              {/* Close Icon */}
+              {/* <svg
+                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div> */}
+
+          <div className="w-full border-t border-gray-300"></div> 
+
+          {/* Desktop Menu */}
+
+          <div className="flex justify-between h-16 py-4">
+            <Link
+              href="/about"
+              className="text-gray-800 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              About
+            </Link>
+
+          </div>
+
+          {/* bottom nav-line */}
+          <div className="w-full border-t border-gray-300"></div> 
+
       </div>
 
       {/* Mobile Menu */}
-      <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
+      {/* <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1">
           <Link
             href="/"
@@ -136,9 +144,9 @@ export default function Navbar() {
             className="block text-gray-800 hover:text-blue-600 px-3 py-2 rounded-md text-base font-medium"
           >
             About
-          </Link>
+          </Link> */}
           {/* Conditional Rendering for Authenticated Users */}
-          {status === "authenticated" ? (
+          {/* {status === "authenticated" ? (
             <>
               <Link
                 href="/dashboard"
@@ -167,8 +175,8 @@ export default function Navbar() {
               Login
             </Link>
           )}
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </nav>
   );
 }
