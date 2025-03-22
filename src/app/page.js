dc0ef03a0b7ace95e81a0c7cac928eb5724de91b
Sujung Song <sujungsong0610@gmail.com>
2025-03-21 21:11:25 -0400

    added main sections

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,48 @@ export default function Dashboard() {
   if (status === "loading") return <p>Loading session...</p>;
 
   return (
-    <div>
+    <>
+      <div>
+
+        <div className="section section-1">
+          <div>
+            <div className="hero-text">Unlock real-time <br/> sensor insights </div>
+            <div className="sub-text"> Streamline your operations, optimize resources, <br/> and gain actionable insights with our intuitive <br/> dashboard and advanced analytics.</div>
+          </div>
+          <div>
+            <img src="/main-unsplash.jpg" alt="Data Sense" className="main-img w-300"/>
+          </div>
+        </div>
+
+          {/* 가로로 3개 요소 추가 */}
+          <div className="section-1">
+            <div className="box">
+              <h2>Real-time Data Insights</h2>
+              <p>Track live data: Monitor temperature, humidity, and moisture levels in real-time, visualize the data through intuitive charts, and gain valuable insights for decision-making.</p>
+            </div>
+            <div className="box">
+              <h2>Instant Access to Key Metrics</h2>
+              <p>Instantly access key data points: Simply plug in a sensor, view live measurements, and download the data for deeper analysis whenever required for optimized results.</p>
+            </div>
+            <div className="box">
+              <h2>Smart Sensor Integration</h2>
+              <p>Easily integrate sensors with Raspberry Pi, track data automatically, and enjoy a seamless, user-friendly experience that simplifies smart sensor management for efficient operations.</p>
+            </div>
+          </div>
+
+
+        <div className="section section-2">
+          <h1>Section 2</h1>
+          <p>This is the second section.</p>
+        </div>
+
+        <div className="section section-3">
+          <h1>Section 3</h1>
+          <p>This is the third section.</p>
+        </div>
+
+      </div>
+    
       {session ? (
         <>
           <h1>Welcome, {session.user.name}!</h1>
@@ -23,6 +64,6 @@ export default function Dashboard() {
           <button onClick={() => signIn()}>Sign In</button>
         </>
       )}
-    </div>
+    </>
   );
 }
