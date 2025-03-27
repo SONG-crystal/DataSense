6aabd07fcc691d71ce88e95d8a5edb9180a4256d
Sujung Song <sujungsong0610@gmail.com>
2025-03-26 20:32:48 -0400

    added section2

--- src/app/page.js
+++ src/app/page.js
@@ -23,31 +23,32 @@ export default function Dashboard() {
           </div>
         </div>
 
-          {/* 가로로 3개 요소 추가 */}
-          <div className="section-1">
-            <div className="box">
-              <h2>Real-time Data Insights</h2>
-              <p>Track live data: Monitor temperature, humidity, and moisture levels in real-time, visualize the data through intuitive charts, and gain valuable insights for decision-making.</p>
-            </div>
-            <div className="box">
-              <h2>Instant Access to Key Metrics</h2>
-              <p>Instantly access key data points: Simply plug in a sensor, view live measurements, and download the data for deeper analysis whenever required for optimized results.</p>
-            </div>
-            <div className="box">
-              <h2>Smart Sensor Integration</h2>
-              <p>Easily integrate sensors with Raspberry Pi, track data automatically, and enjoy a seamless, user-friendly experience that simplifies smart sensor management for efficient operations.</p>
-            </div>
+        <div className="section-1">
+          <div className="box">
+            <h2>Real-time Data Insights</h2>
+            <p>Track live data: Monitor temperature, humidity, and moisture levels in real-time, visualize the data through intuitive charts, and gain valuable insights for decision-making.</p>
           </div>
-
-
-        <div className="section section-2">
-          <h1>Section 2</h1>
-          <p>This is the second section.</p>
+          <div className="box">
+            <h2>Instant Access to Key Metrics</h2>
+            <p>Instantly access key data points: Simply plug in a sensor, view live measurements, and download the data for deeper analysis whenever required for optimized results.</p>
+          </div>
+          <div className="box">
+            <h2>Smart Sensor Integration</h2>
+            <p>Easily integrate sensors with Raspberry Pi, track data automatically, and enjoy a seamless, user-friendly experience that simplifies smart sensor management for efficient operations.</p>
+          </div>
         </div>
 
-        <div className="section section-3">
-          <h1>Section 3</h1>
-          <p>This is the third section.</p>
+        <div className="section section-2">
+          <div>
+            <img src="/new-oil.jpg" alt="Data Sense" className="section2-img h-170"/>
+          </div>
+          <div>
+            <div className="hero-text">“DATA IS THE NEW OIL”   
+              <span className="quote-author">Clive Humby</span>
+            </div>
+            <div className="author-text">   </div>
+            <div className="sub-text"> Turn data into your most valuable asset, <br/> optimize operations and drive impact with actionable insights. <br/>  </div>
+          </div>
         </div>
 
       </div>
