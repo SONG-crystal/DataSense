faaf867ee94c0dac119092f00cde91cc2e21d1ab
Sujung Song <sujungsong0610@gmail.com>
2025-04-01 19:58:28 -0400

    updated media query on device tab

--- src/app/devices/page.js
+++ src/app/devices/page.js
@@ -2,7 +2,6 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useDevices } from "@/app/context/deviceContext";
-import DeviceCard from "@/app/components/cards/device";
 import Image from 'next/image';
 
 export default function Devices() {
@@ -19,27 +18,27 @@ export default function Devices() {
   }
 
   return (
-    <div className="flex flex-wrap justify-left gap-6 p-10 bg-gray-100 min-h-screen">
+    <div className="device flex flex-wrap justify-left gap-6 p-10 bg-gray-100 min-h-screen">
       <div className="w-full max-w-4xl">
         {devices.map((device, index) => (
           <div key={index} className="flex-none">
   
-            <div className="card flex bg-white rounded-lg shadow-lg max-w-4xl w-full ml-30">
+            <div className="card flex bg-white rounded-lg shadow-lg max-w-4xl w-full ">
               <div className="image-container flex-shrink-0">
                 <Image
                   src="/device.jpg"
                   alt="image"
                   width={300}
                   height={200}
-                  className="rounded-l-lg"
+                  className="device-img rounded-l-lg"
                 />
               </div>
-              <div className="mt-3">
-                <p className="text-lg text-gray-700"> <span className="font-bold inline-block w-35">User Name:</span> {session.user.name}</p>
-                <p className="text-lg text-gray-700"> <span className="font-bold inline-block w-35">Capacity:</span> 128GB </p>
-                <p className="text-lg text-gray-700"> <span className="font-bold inline-block w-37">Version: </span>2.1.0</p>
-                <p className="text-lg text-gray-700"> <span className="font-bold inline-block w-35">Identifier:</span> {device.serialId} </p>
-                <p className="text-lg text-gray-700"> <span className="font-bold inline-block w-35">Device Name:</span> {device.name}</p>
+              <div className="mt-3 text-lg text-gray-700 p-4">
+                <p> <span className="font-bold inline-block w-35">User Name:</span> {session.user.name}</p>
+                <p> <span className="font-bold inline-block w-35">Capacity:</span>   32GB </p>
+                <p> <span className="font-bold inline-block w-35">Version: </span>   1.24.0</p>
+                <p> <span className="font-bold inline-block w-35">Identifier:</span> {device.serialId} </p>
+                <p> <span className="font-bold inline-block w-35">Device Name:</span> {device.name}</p>
               </div>
             </div>
           </div>
